416998867739e01ccf2e6f43cb322a2141cd6572
Sebastian Pagel <mail@sebastian-pagel.net>
2021-01-23 00:27:00 +0100

    handle ParamPoly3 pRange_normalized different - update poly coefficients

--- Geometries/ParamPoly3.cpp
+++ Geometries/ParamPoly3.cpp
@@ -26,14 +26,20 @@ ParamPoly3::ParamPoly3(double s0,
     RoadGeometry(s0, x0, y0, hdg0, length, GeometryType::ParamPoly3),
     aU(aU), bU(bU), cU(cU), dU(dU), aV(aV), bV(bV), cV(cV), dV(dV), pRange_normalized(pRange_normalized)
 {
+    if (!pRange_normalized)
+    {
+        this->bU = bU * length;
+        this->bV = bV * length;
+        this->cU = cU * length * length;
+        this->cV = cV * length * length;
+        this->dU = dU * length * length * length;
+        this->dV = dV * length * length * length;
+    }
 }
 
 Vec2D ParamPoly3::get_xy(double s) const
 {
-    double p = (s - s0);
-    if (this->pRange_normalized)
-        p /= length;
-
+    const double p = (s - s0) / length;
     const double xs = aU + bU * p + cU * p * p + dU * p * p * p;
     const double ys = aV + bV * p + cV * p * p + dV * p * p * p;
     const double xt = (std::cos(hdg0) * xs) - (std::sin(hdg0) * ys) + x0;
@@ -46,11 +52,7 @@ Vec2D ParamPoly3::get_grad(double s) const
 {
     const double h1 = std::cos(hdg0);
     const double h2 = std::sin(hdg0);
-
-    double p = (s - s0);
-    if (this->pRange_normalized)
-        p /= length;
-
+    const double p = (s - s0) / length;
     const double dx = h1 * (bU + 2 * cU * p + 3 * dU * p * p) - h2 * (bV + 2 * cV * p + 3 * dV * p * p);
     const double dy = h2 * (bU + 2 * cU * p + 3 * dU * p * p) + h1 * (bV + 2 * cV * p + 3 * dV * p * p);
 
@@ -81,7 +83,6 @@ std::vector<double> ParamPoly3::approximate_linear(double eps) const
         std::vector<double> p_vals;
         for (double p = 0; p < 1.0; p += step_size)
             p_vals.push_back(p);
-        p_vals.push_back(1.0);
         return p_vals;
     };
 
@@ -116,10 +117,10 @@ std::vector<double> ParamPoly3::approximate_linear(double eps) const
         for (const double& p_sub : approximate_linear_quad(pM_quad, pB_quad_1, pD_sub, eps))
             p_vals.push_back(p0 + (p1 - p0) * 0.5 + p_sub * (p1 - p0) * 0.5);
     }
+    p_vals.push_back(1.0);
 
     for (double& p : p_vals)
-        p = this->pRange_normalized ? p * length + s0 : p + s0;
-
+        p = p * length + s0;
     return p_vals;
 }
 
