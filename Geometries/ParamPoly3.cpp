0ec66ac02c7cd1821e879ff4c28129d87ccd7bdc
Sebastian Pagel <mail@sebastian-pagel.net>
2021-10-10 14:49:03 +0200

    fix t=1.0 to the specified ParamPoly3::length after approximation

--- Geometries/ParamPoly3.cpp
+++ Geometries/ParamPoly3.cpp
@@ -38,6 +38,7 @@ ParamPoly3::ParamPoly3(double s0,
 
     const std::array<Vec2D, 4> coefficients = {{{this->aU, this->aV}, {this->bU, this->bV}, {this->cU, this->cV}, {this->dU, this->dV}}};
     this->cubic_bezier = CubicBezier2D(CubicBezier2D::get_control_points(coefficients));
+    this->cubic_bezier.arclen_t[length] = 1.0;
 }
 
 Vec2D ParamPoly3::get_xy(double s) const
