7550852dd6d7dbb3346f29e93715b6c34ae12866
Sebastian Pagel <mail@sebastian-pagel.net>
2021-11-05 19:03:57 +0100

    exclude Viewer/libOpenDrive.js from formatting

--- format-files.sh
+++ format-files.sh
@@ -4,4 +4,5 @@ find -E . -type f \
     -regex '.*\.(cpp|c|hpp|h|js)' \
     -not -path "./build/*" \
     -not -path "./Thirdparty/*" \
+    -not -name './Viewer/libOpenDrive.js' \
     -exec clang-format --style=file -i '{}' \;
