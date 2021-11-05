240773d0ea06e2b38164a0bb68adde31197037c3
Sebastian Pagel <mail@sebastian-pagel.net>
2021-11-05 18:54:17 +0100

    format *.js files based on WebKit style

--- format-files.sh
+++ format-files.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 find -E . -type f \
-    -regex '.*\.(cpp|c|hpp|h)' \
+    -regex '.*\.(cpp|c|hpp|h|js)' \
     -not -path "./build/*" \
     -not -path "./Thirdparty/*" \
     -exec clang-format --style=file -i '{}' \;
