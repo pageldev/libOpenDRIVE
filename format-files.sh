acc93d68884b656cf14178357e165360ccbf7e9c
Sebastian Pagel <mail@sebastian-pagel.net>
2022-04-01 16:57:15 +0200

    update format-files.sh, ignore ModuleOpenDrive.js

--- format-files.sh
+++ format-files.sh
@@ -4,6 +4,6 @@ find -E . -type f \
     -regex '.*\.(cpp|c|hpp|h|js)' \
     -not -path "./build/*" \
     -not -path "./Thirdparty/*" \
-    -not -name './Viewer/libOpenDrive.js' \
+    -not -name './Viewer/ModuleOpenDrive.js' \
     -not -path './Viewer/lib/*' \
     -exec clang-format --style=file -i '{}' \;
