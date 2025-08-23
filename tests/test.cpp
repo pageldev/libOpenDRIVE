f41ad78307a2e4955a7b2145480d9a72aaebd170
Sebastian Pagel <mail@sebastian-pagel.net>
2025-08-23 12:05:15 +0000

    test - refactor, use TEST_CASE_METHOD for basic OpenDriveMap test case

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,32 +1,40 @@
 #include "OpenDriveMap.h"
 #include <catch2/catch_test_macros.hpp>
-#include <iostream>
 
-TEST_CASE("Basic OpenDriveMap check", "[xodr]")
+struct OpenDriveFixture
 {
-    odr::OpenDriveMap odr_map("test.xodr");
-    REQUIRE(odr_map.xml_parse_result);
-    REQUIRE(!odr_map.get_roads().empty());
+    OpenDriveFixture() : odr_map("test.xodr")
+    {
+        REQUIRE(odr_map.xml_parse_result);
+        REQUIRE(!odr_map.get_roads().empty());
+    }
 
-    // test routing
+    odr::OpenDriveMap odr_map;
+};
+
+TEST_CASE_METHOD(OpenDriveFixture, "Basic OpenDriveMap check", "[xodr]")
+{
+    // basic routing test
     auto graph = odr_map.get_routing_graph();
     auto path = graph.shortest_path(odr::LaneKey("43", 0.0, 1), odr::LaneKey("41", 0.0, 1));
     REQUIRE(path.size() == 15);
 
+    // road sanity
     for (const odr::Road& road : odr_map.get_roads())
     {
         INFO("road: " << road.id << ", length: " << road.length);
         REQUIRE(road.length >= 0.0);
         REQUIRE(!road.s_to_lanesection.empty());
-        for (const odr::LaneSection& lanesection : road.get_lanesections())
+        for (const odr::LaneSection& ls : road.get_lanesections())
         {
-            const double s_start = lanesection.s0;
-            const double s_end = road.get_lanesection_end(lanesection);
+            const double s_start = ls.s0;
+            const double s_end = road.get_lanesection_end(ls);
             REQUIRE(s_start >= 0.0);
             REQUIRE(s_end > s_start);
-            for (const odr::Lane& lane : lanesection.get_lanes())
+            for (const odr::Lane& lane : ls.get_lanes())
             {
                 std::vector<odr::RoadMark> roadmarks = lane.get_roadmarks(s_start, s_end);
+                (void)roadmarks; // silence unused var if not checked
             }
         }
     }
